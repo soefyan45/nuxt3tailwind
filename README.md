b7317850e09c9ba5ee0c2cebe7eaf2ab4020e49d
tukangpel <soefyan45@gmail.com>
2021-11-16 13:43:15 +0700

    Nuxt3 Tailwindcss

---
+++ README.md
@@ -0,0 +1,29 @@
+# Nuxt 3 Minimal Starter With TailwindCSS
+
+We recommend to look at the [documentation](https://v3.nuxtjs.org).
+
+## Setup
+
+Make sure to install the dependencies
+
+```bash
+yarn install
+```
+
+## Development
+
+Start the development server on http://localhost:3000
+
+```bash
+yarn dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+yarn build
+```
+
+Read More  [soefyansyah.com](https://soefyansyah.com/blog/install-tailwindcss-di-nuxt3-dengan-mudah).
---
+++ app.vue
@@ -0,0 +1,18 @@
+<!-- <script lang="ts" setup>
+  import './assets/css/tailwind.css'
+</script> -->
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div class="h-full">
+      <div class="h-screen w-full bg-gradient-to-r from-gray-900 via-gray-800 to-blue-700">
+        <div class="w-full lg:w-4/12 flex h-screen items-center">
+        <div class="flex flex-col px-6 my-auto items-start">
+          <div class="text-5xl lg:text-9xl font-bold text-white">Hallo</div>
+          <div class="text-xl lg:text-4xl font-semibold text-gray-300">I'm Nuxt Paired Tailwind</div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
